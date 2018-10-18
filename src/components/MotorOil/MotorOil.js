a7ce38c5c8a7d35bceeadd28fe394d4e38f0dd6d
AhmedShab <a.shaban@qetaa.com>
2018-10-18 13:55:38 +0300

    moved common methods to hoc

--- src/components/MotorOil/MotorOil.js
+++ src/components/MotorOil/MotorOil.js
@@ -11,7 +11,7 @@ import WithProductView from '../../hoc/WithProductView';
 import { Checkbox } from 'primereact/components/checkbox/Checkbox';
 import queryString from 'qs';
 
-import { isEmpty, upperCaseFirstChar, replaceAll } from '../../utils';
+import { isEmpty, replaceAll } from '../../utils';
 
 import './MotorOil.css';
 
@@ -22,16 +22,9 @@ const price = 'price';
 const rating = 'rating';
 
 class MotorOil extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			filtration: [],
-			params: ''
-		}
-	}
 
 	componentDidMount() {
-		const { location: { search } } = this.props;
+		const { location: { search }, filterObject } = this.props;
 		const query = queryString.parse(search.slice(1));
 		const keys = Object.keys(query);
 
@@ -47,10 +40,9 @@ class MotorOil extends Component {
 		});
 
 		const newParams = search.slice(1).split(/[&]/).filter(param => !param.includes(','));
-		
 
-		this.setState({ params: '?'.concat(newParams.join('&')) });
-		return this.addToFilter(filters);
+		this.props.onSetParams(newParams)
+		return this.props.onAddToFilter(filters, filterObject);
 
 	}
 
@@ -59,77 +51,9 @@ class MotorOil extends Component {
 
 		if (search !== prevProps.location.search) {
 
-		} else if (this.state.params !== prevState.params) {
-			history.push(`${match.url}${this.state.params}`);
+		} else if (this.props.params !== prevProps.params) {
+			history.push(`${match.url}${this.props.params}`);
 		}
-	}
-
-	addToFilter = (filters) => {
-		let params = [];
-		const { filterObject } = this.props;
-
-		filters.forEach(data => {
-			data.values.forEach(value => {
-				const matchValues = filterObject[data.label] ? filterObject[data.label].values.includes(value) : null;
-				const keyExists = filterObject[data.label];
-				if (matchValues && keyExists) {
-					params.push(`${upperCaseFirstChar(data.label).replace('_', ' ')} ${value}`);
-				}
-			});
-		});
-
-
-		this.setState({ filtration: params })
-	}
-
-	filter = (item, event) => {
-		const { value, checked } = event;
-		const index = this.state.filtration.indexOf(value);
-		const itemValue = item.value.replace(/ /, '_');
-
-		if (checked && index === -1) {
-			const newParams = this.state.params.includes('?') ? this.state.params.concat(`&${item.key}=${itemValue}`) : this.state.params.concat(`?${item.key}=${itemValue}`);
-			this.setState({
-				filtration: [...this.state.filtration, value],
-				params: newParams
-			});
-		} else if (index !== -1) {
-			const clone = [...this.state.filtration];
-			const newParams = this.state.params.slice(1).split(/[&]/);
-
-			clone.splice(index, 1);
-			newParams.splice(index, 1);
-			this.setState({
-				filtration: clone,
-				params: '?'.concat(newParams.join('&'))
-			});
-		}
-	}
-
-	removeItem = (index, event) => {
-		event.preventDefault();
-		const clone = [...this.state.filtration];
-		const newParams = this.state.params.slice(1).split(/[&]/);
-
-		clone.splice(index, 1);
-		newParams.splice(index, 1);
-		
-		this.setState({
-			filtration: clone,
-			params: '?'.concat(newParams.join('&'))
-		});
-	}
-
-	isChecked = (value) => {
-		const check = this.state.filtration.find(result => result === value);
-
-		return check ? true : false;
-	}
-
-	handleClear = () => {
-		this.setState({
-			filtration: []
-		})
 	}
 
 	render() {
@@ -138,7 +62,7 @@ class MotorOil extends Component {
 				width: '5em'
 			}
 		}
-		const { filterObject } = this.props;
+		const { filterObject, isChecked, renderSearch, filtration, onFilter, onRemoveItem, onClear } = this.props;
 
 		return (
 			<Fragment>
@@ -149,10 +73,10 @@ class MotorOil extends Component {
 					<label htmlFor="">Sort by</label>
 					<Select options={categorySortOptions} onChange={this.props.handleSelectChange} />
 				</div>
-				<div style={isEmpty(this.state.filtration) ? styles.hide : styles.grey}>
+				<div style={isEmpty(filtration) ? styles.hide : styles.grey}>
 					{
-						this.state.filtration.map((item, index) => (
-							<label key={index} style={{ ...styles.listingPage.searchResult, ...styles.rightSpace }} onClick={this.removeItem.bind(this, index)}>{item}</label>
+						filtration.map((item, index) => (
+							<label key={index} style={{ ...styles.listingPage.searchResult, ...styles.rightSpace }} onClick={onRemoveItem.bind(this, index)}>{item}</label>
 						))
 					}
 				</div>
@@ -160,16 +84,16 @@ class MotorOil extends Component {
 					<div className="MotorOil-filter border rounded card">
 						<p>{filterObject.viscosity_grade.label}</p>
 						<input className="form-control" type="text" name="" id="" placeholder="Search" />
-						{this.props.renderSearch({ filtration: filterObject.viscosity_grade, key: viscosity_grade }, Checkbox, this.filter, this.isChecked)}
+						{renderSearch({ filtration: filterObject.viscosity_grade, key: viscosity_grade }, Checkbox, onFilter, isChecked)}
 						<hr />
 						<p>{filterObject.volume.label}</p>
-						{this.props.renderSearch({ filtration: filterObject.volume, key: volume }, Checkbox, this.filter, this.isChecked)}
+						{renderSearch({ filtration: filterObject.volume, key: volume }, Checkbox, onFilter, isChecked)}
 						<hr />
 						<p>{filterObject.brand.label}</p>
-						{this.props.renderSearch({ filtration: filterObject.brand, key: brand }, Checkbox, this.filter, this.isChecked)}
+						{renderSearch({ filtration: filterObject.brand, key: brand }, Checkbox, onFilter, isChecked)}
 						<hr />
 						<p>{filterObject.price.label}</p>
-						{this.props.renderSearch({ filtration: filterObject.price, key: price }, Checkbox, this.filter, this.isChecked)}
+						{renderSearch({ filtration: filterObject.price, key: price }, Checkbox, onFilter, isChecked)}
 						<div className="MotorOil-filter_price">
 							<input style={buttonsStyle.price} className="form-control" type="text" placeholder="Min" name="" id="" />
 							<input style={buttonsStyle.price} className="form-control" type="text" placeholder="Max" name="" id="" />
@@ -177,9 +101,9 @@ class MotorOil extends Component {
 						</div>
 						<hr />
 						<p>{filterObject.rating.label}</p>
-						{this.props.renderSearch({ filtration: filterObject.rating, key: rating }, Checkbox, this.filter, this.isChecked)}
+						{renderSearch({ filtration: filterObject.rating, key: rating }, Checkbox, onFilter, isChecked)}
 						<hr />
-						<Button text="Clear all" className="btn btn-secondary" onClick={this.handleClear} />
+						<Button text="Clear all" className="btn btn-secondary" onClick={onClear} />
 					</div>
 					<div className="MotorOil-contents">
 						<ProductGridView currentProducts={this.props.currentProducts} />
--- src/hoc/WithProductView/index.js
+++ src/hoc/WithProductView/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { upperCaseFirstChar } from '../../utils'
 
 const rows = 12;
 const recommended = 'recommended';
@@ -12,6 +13,8 @@ const WithProductView = WrappedComponent => {
 			super(props);
 
 			this.state = {
+				filtration: [],
+				params: '',
 				currentProducts: this.getCurrentProducts(0),
 				first: 0,
 				rows,
@@ -41,6 +44,70 @@ const WithProductView = WrappedComponent => {
 			});
 		}
 
+		addToFilter = (filters, filterObject) => {
+			let params = [];
+
+			filters.forEach(data => {
+				data.values.forEach(value => {
+					const matchValues = filterObject[data.label] ? filterObject[data.label].values.includes(value) : null;
+					const keyExists = filterObject[data.label];
+					if (matchValues && keyExists) {
+						params.push(`${upperCaseFirstChar(data.label).replace('_', ' ')} ${value}`);
+					}
+				});
+			});
+
+
+			this.setState({ filtration: params })
+		}
+
+		filter = (item, event) => {
+			const { value, checked } = event;
+			const index = this.state.filtration.indexOf(value);
+			const itemValue = item.value.replace(/ /, '_');
+
+			if (checked && index === -1) {
+				console.log(this.state.params);
+				
+				const newParams = this.state.params.length === 1 ? this.state.params.concat(`${item.key}=${itemValue}`) : this.state.params.concat(`&${item.key}=${itemValue}`);
+				
+				this.setState({
+					filtration: [...this.state.filtration, value],
+					params: newParams
+				});
+			} else if (index !== -1) {
+				const clone = [...this.state.filtration];
+				const newParams = this.state.params.slice(1).split(/[&]/);				
+
+				clone.splice(index, 1);
+				newParams.splice(index, 1);
+				this.setState({
+					filtration: clone,
+					params: fixParamsFormat(newParams)
+				});
+			}
+		}
+
+		removeItem = (index, event) => {
+			event.preventDefault();
+			const clone = [...this.state.filtration];
+			const newParams = this.state.params.slice(1).split(/[&]/);
+
+			clone.splice(index, 1);
+			newParams.splice(index, 1);
+
+			this.setState({
+				filtration: clone,
+				params: fixParamsFormat(newParams)
+			});
+		}
+
+		isChecked = (value) => {
+			const check = this.state.filtration.find(result => result === value);
+
+			return check ? true : false;
+		}
+
 		renderSearch = ({ filtration, key }, Component, handleChange, isChecked) => {
 			return filtration.values.map((value, index) => (
 				<div key={index}>
@@ -48,6 +115,12 @@ const WithProductView = WrappedComponent => {
 					<label>{value}</label>
 				</div>
 			))
+		}
+
+		handleClear = () => {
+			this.setState({
+				filtration: []
+			})
 		}
 
 		handleSelectChange = (selectedOption) => {
@@ -63,6 +136,10 @@ const WithProductView = WrappedComponent => {
 			}
 		}
 
+		setParams = (newParams) => {
+			this.setState({ params: fixParamsFormat(newParams) });
+		}
+
 		render() {
 			return <WrappedComponent
 				currentProducts={this.state.currentProducts}
@@ -72,9 +149,21 @@ const WithProductView = WrappedComponent => {
 				onPageChange={this.onPageChange}
 				handleSelectChange={this.handleSelectChange}
 				renderSearch={this.renderSearch}
+				onAddToFilter={this.addToFilter}
+				onFilter={this.filter}
+				onRemoveItem={this.removeItem}
+				isChecked={this.isChecked}
+				onClear={this.handleClear}
+				onSetParams={this.setParams}
+				
+				{...this.state}
 				{...this.props} />
 		}
 	}
+}
+
+const fixParamsFormat = (newParams) => {
+	return '?'.concat(newParams.join('&'))
 }
 
 export default WithProductView;
