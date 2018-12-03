a8062bcde08f2d8cab2e236ebb86952fbb4fb82e
AhmedShab <a.shaban@qetaa.com>
2018-12-03 16:32:25 +0300

    fixed items style in cart

--- src/components/RenderCartItem/RenderCartItem.js
+++ src/components/RenderCartItem/RenderCartItem.js
@@ -1,61 +1,117 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Field } from 'redux-form'
 import RenderField from '../RenderField/RenderField';
 import Button from '../UI/Button';
 import * as validations from '../../utils';
 import NumberPicker from '../UI/NumberPicker';
-
-import './RenderCartItem.css';
+import RenderImage from '../RenderImage/RenderImage';
+// import './RenderCartItem.css';
 
 export default class extends Component {
 
- componentWillMount() {
-  const { fields, purchasedItems } = this.props;
-
-  purchasedItems.map(cartItem => {
-   return fields.push({
-    itemName: cartItem.desc,
-    quantity: cartItem.quantity,
-    price: cartItem.price,
-   })
-  })
- }
+    componentWillMount() {
+        const { fields, purchasedItems } = this.props;
+        console.log(purchasedItems);
 
 
- render() {
-  const { fields, meta: { error, submitFailed }, deleteText } = this.props;
-  return (
-   <div>
-    {submitFailed && error && <span>{error}</span>}
-    {fields.map((cartItem, idx) => (
-     <div key={idx} className="RenderCartItem-box_item card card-body">
-      <Field
-       className="RenderCartItem-input"
-       name={`${cartItem}.itemName`}
-       type="text"
-       component={RenderField}
-       readOnly
-       validate={[validations.required]}
-      />
-      <Field
-       name={`${cartItem}.quantity`}
-       component={NumberPicker}
-      />
-      <Field
-       className="RenderCartItem-input"
-       name={`${cartItem}.price`}
-       readOnly
-       component={RenderField}
-      />
-      <Button
-       type="button"
-       className="btn btn-light"
-       text={deleteText}
-       onClick={() => fields.remove(idx)}
-      />
-     </div>
-    ))}
-   </div>
-  )
- }
+        purchasedItems.map(cartItem => {
+            return fields.push({
+                itemName: cartItem.desc,
+                quantity: cartItem.quantity,
+                quantityLabel: cartItem.quantityLabel,
+                price: cartItem.price,
+                currency: cartItem.currency,
+                image: cartItem.image,
+                productNumber: cartItem.productNumber,
+                manufacturerName: cartItem.manufacturerName
+            })
+        })
+    }
+
+
+    render() {
+        const { fields, meta: { error, submitFailed }, deleteText } = this.props;
+        return (
+            <Fragment>
+                {submitFailed && error && <span>{error}</span>}
+                {fields.map((cartItem, idx) => {
+                    console.log(`${cartItem}.itemName`)
+                    return <div key={idx} className="border rounded card">
+                        <div class="row">
+                            <div className="col-5 col-md-2">
+                                <Field
+                                    name={`${cartItem}.image`}
+                                    component={RenderImage}
+                                />
+                            </div>
+                            <div className="col-7 col-md-4 pt">
+                                <div className="cart_product-details">
+                                    <Field
+                                        className="part-text"
+                                        name={`${cartItem}.itemName`}
+                                        type="text"
+                                        component={RenderField}
+                                        readOnly
+                                    />
+                                    <Field
+                                        className="manufacturer-text"
+                                        name={`${cartItem}.manufacturerName`}
+                                        type="text"
+                                        component={RenderField}
+                                        readOnly
+                                    />
+                                    <Field
+                                        className="part-text"
+                                        name={`${cartItem}.productNumber`}
+                                        type="text"
+                                        component={RenderField}
+                                        readOnly
+                                    />
+                                    <div className="w-100">
+                                        <Field
+                                            className="sales-price"
+                                            name={`${cartItem}.price`}
+                                            readOnly
+                                            component={RenderField}
+                                        />
+                                        <Field
+                                            className="currency"
+                                            name={`${cartItem}.currency`}
+                                            readOnly
+                                            component={RenderField}
+                                        />
+                                    </div>
+                                    <Button
+                                        type="button"
+                                        className="btn-secondary"
+                                        text={"Move to wishlist"}
+                                    />
+                                    <Button
+                                        type="button"
+                                        className="btn-secondary"
+                                        text={deleteText}
+                                        onClick={() => fields.remove(idx)}
+                                    />
+                                </div>
+                            </div>
+                            <div className="col-12 col-md-6 pt">
+                                <Field
+                                    className="part-text"
+                                    name={`${cartItem}.quantityLabel`}
+                                    type="text"
+                                    component={RenderField}
+                                    readOnly
+                                />
+                                <Field
+                                    name={`${cartItem}.quantity`}
+                                    component={NumberPicker}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                }
+                )}
+            </Fragment>
+        )
+    }
 }
