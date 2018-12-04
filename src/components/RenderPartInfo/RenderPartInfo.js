d8cf42fe7cce3a560412f50bf48b1f6ff30f50fa
qetaa <fareed@qetaa.com>
2018-12-04 18:31:01 +0300

    custom order part container finished

--- src/components/RenderPartInfo/RenderPartInfo.js
+++ src/components/RenderPartInfo/RenderPartInfo.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Field } from 'redux-form'
 import RenderField from '../RenderField/RenderField';
 import Button from '../UI/Button';
@@ -6,32 +6,43 @@ import * as validations from '../../utils';
 import NumberPicker from '../UI/NumberPicker';
 import RenderFileInput from '../RenderFileInput/RenderFileInput';
 
-export default ({ fields, meta: { error, submitFailed }, ...props }) => (
- <div>
-  <Button type="reset" className="btn" text={props.add}
-  onClick={() => fields.push({
-   itemName: '',
-   image: '',
-   // condition: '',
-   quantity: 1
-  })}
-  />
-  {submitFailed && error && <span>{error}</span>}
-  {fields.map((partInfo, idx) => (
-   <div key={idx} className="QuotationRequest-box_item">
-    <Field
-     name={`${partInfo}.image`}
-     image={`${partInfo}.image`}
-     component={RenderFileInput}
-    />
-    <Field
-     name={`${partInfo}.itemName`}
-     type="text"
-     component={RenderField}
-     placeholder={props.placeholder}
-     validate={[validations.required]}
-    />
-    {/* <Field
+class RenderPartInfo extends Component {
+
+    componentWillMount() {
+        const { fields } = this.props;
+        fields.push({
+            itemName: '',
+            image: '',
+            quantity: 1
+        });
+    }
+
+    render() {
+        const { fields, meta: { error, submitFailed } } = this.props;
+        return (
+            <div>
+                {submitFailed && error && <span>{error}</span>}
+                {fields.map((partInfo, idx) => (
+                    <div key={idx} className="row parts-container">
+                        
+                        <div className="col-8">
+                            <Field
+                                className="select-field-make"
+                                name={`${partInfo}.itemName`}
+                                type="text"
+                                component={RenderField}
+                                placeholder={this.props.placeholder}
+                                validate={[validations.required]}
+                            />
+
+                            <Field
+                            name={`${partInfo}.image`}
+                            image={`${partInfo}.image`}
+                            component={RenderFileInput}
+                        />
+                        </div>
+
+                        {/* <Field
      name={`${partInfo}.condition`}
      className="QuotationRequest-selectInput"
      component={SelectInput}
@@ -41,17 +52,35 @@ export default ({ fields, meta: { error, submitFailed }, ...props }) => (
       { value: 'all ', label: 'All' }
      ]}
     /> */}
-    <Field
-     name={`${partInfo}.quantity`}
-     component={NumberPicker}
-    />
-    <Button
-     type="button"
-     className="btn btn-light"
-     text={props.deleteButton}
-     onClick={() => fields.remove(idx)}
-    />
-   </div>
-  ))}
- </div>
-)
+                        <div className="col-3">
+                            <Field
+                                name={`${partInfo}.quantity`}
+                                component={NumberPicker}
+                            />
+                        </div>
+                        <div className="col-1">
+                            <Button
+                                type="button"
+                                className="btn btn-light"
+                                text={this.props.deleteButton}
+                                onClick={() => fields.remove(idx)}
+                            />
+                        </div>
+                    </div>
+                ))}
+                <div className="add-part-btn-container">
+                    <Button type="reset" className="btn" text={this.props.add}
+                        onClick={() => fields.push({
+                            itemName: '',
+                            image: '',
+                            // condition: '',
+                            quantity: 1
+                        })}
+                    />
+                </div>
+            </div>
+        )
+    }
+
+}
+export default RenderPartInfo;
