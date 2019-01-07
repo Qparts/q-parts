b8c16e183f756f452f32aef93f83be2ac6e1583c
raedawwad95 <raed@qetaa.com>
2019-01-07 17:01:41 +0200

    fixed style

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -36,8 +36,8 @@ class Payment extends Component {
         <p>SHAIMAA AHMED M SOUDY</p>
       </div>
       <div className="visa-num">
-        <p className="end-number">Number Ending <p>4871</p></p>
-        <p className="expires-date">Expires<p>04/2023</p></p>
+        <p className="end-number">Number Ending <span>4871</span></p>
+        <p className="expires-date">Expires<span>04/2023</span></p>
       </div>
       <div className="payment-footer">
        <Button type="button" className="btn btn-link" text="Edit" icon="icon-edit" isReverseOrder/>
@@ -59,8 +59,8 @@ class Payment extends Component {
          <p>SHAIMAA AHMED M SOUDY</p>
        </div>
        <div className="visa-num">
-         <p className="end-number">Number Ending <p>4871</p></p>
-         <p className="expires-date">Expires<p>04/2023</p></p>
+         <p className="end-number">Number Ending <span>4871</span></p>
+         <p className="expires-date">Expires<span>04/2023</span></p>
        </div>
       <div className="payment-footer">
        <Button type="button" className="btn btn-link" text="Edit" icon="icon-edit" isReverseOrder/>
