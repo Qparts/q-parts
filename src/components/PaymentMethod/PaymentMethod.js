c80fbf297a340802ccf23752ff6eee425346ece1
raedawwad95 <raed@qetaa.com>
2019-01-27 16:18:55 +0200

    checkout payment method

--- src/components/PaymentMethod/PaymentMethod.js
+++ src/components/PaymentMethod/PaymentMethod.js
@@ -5,13 +5,25 @@ import './PaymentMethod.css';
 
 class PaymentMethod extends Component {
  render() {
+   const { translate } = this.props;
   return (
    <div>
     <div className="PaymentMethod_subheader">
      <h4>{this.props.title}</h4>
-     <Button type="button" className="btn btn-link" text={this.props.change} />
-    </div>
-    <p>{this.props.paymentMethod.type}</p>
+       <div className="payment-box_item">
+        <div className="payment-box_item-label">
+          <img className="main-img" alt="user" src="/img/visa.svg" />
+          <p>SHAIMAA AHMED M SOUDY</p>
+        </div>
+        <div className="visa-num">
+          <p className="end-number">Number Ending <span>4871</span></p>
+          <p className="expires-date">Expires<span>04/2023</span></p>
+        </div>
+        <div className="payment-footer">
+         <Button type="button" className="btn btn-link" text="Edit" icon="icon-edit" isReverseOrder/>
+        </div>
+       </div>
+      </div>
    </div>
   )
  }
