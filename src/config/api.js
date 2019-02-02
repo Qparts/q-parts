59504b06eb47775bfe45866096a40819abbb86b8
AhmedShab <a.shaban@qetaa.com>
2019-02-03 01:14:55 +0300

    using the server

--- src/config/api.js
+++ src/config/api.js
@@ -3,7 +3,7 @@ const proxyHost = 'qtest.fareed9.com';
 
 const hostname = window && window.location && window.location.hostname;
 
-if (false) {
+if (true) {
  backendHost = `http://${proxyHost}`;
 } else {
  backendHost = 'http://localhost:8000';
