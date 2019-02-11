597d04f207cb735d380f35812b45c061addda5f6
AhmedShab <a.shaban@qetaa.com>
2019-02-11 09:27:50 +0300

    fixed the urls after the user logs in

--- src/components/UI/Nav/index.js
+++ src/components/UI/Nav/index.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { Link } from "react-router-dom";
+import { closeNav } from '../../../utils';
 
 const Nav = ({ translate, isLoggedIn, fullName }) => {
     return (
@@ -21,10 +22,10 @@ const Nav = ({ translate, isLoggedIn, fullName }) => {
                         {
                             isLoggedIn ?
                                 <Fragment>
-                                    <li><Link to="/setting/addresses">{translate("navBar.menu.menuItem.address")}</Link></li>
-                                    <li><Link to="/setting/quotations">{translate("navBar.menu.menuItem.quotations")}</Link></li>
-                                    <li><Link to="/setting">{translate("navBar.menu.menuItem.setting")}</Link></li>
-                                    <li><Link to="/logout">{translate("navBar.menu.menuItem.logout")}</Link></li>
+                                    <li><Link to="/setting/addresses" onClick={close}>{translate("navBar.menu.menuItem.address")}</Link></li>
+                                    <li><Link to="/setting/quotations" onClick={close}>{translate("navBar.menu.menuItem.quotations")}</Link></li>
+                                    <li><Link to="/setting" onClick={close}>{translate("navBar.menu.menuItem.setting")}</Link></li>
+                                    <li><Link to="/logout" onClick={close}>{translate("navBar.menu.menuItem.logout")}</Link></li>
                                 </Fragment> :
                                 <div>
                                     <div className="user mx-3">
@@ -33,14 +34,14 @@ const Nav = ({ translate, isLoggedIn, fullName }) => {
                                     </div>
                                     <div>
                                         <div className="signin-list">
-                                            <li><Link className="btn" to="/login" >{translate("general.signin")} <i className={`icon-arrow-right`} /></Link></li>
+                                            <li><Link className="btn" to="/login" onClick={close}>{translate("general.signin")} <i className={`icon-arrow-right`} /></Link></li>
                                             <li><Link to="#"><i className="icon-facebook" /></Link></li>
                                             <li><Link to="#"><img src="/img/google-icon.svg" alt="google button"></img></Link></li>
                                         </div>
                                     </div>
                                     <p className="px-3">
                                         {translate("dropdown.signup.message")}
-                                        <Link to="/signup">
+                                        <Link to="/signup" onClick={close}>
                                             {translate("dropdown.signup.link")}
                                             <i className={`icon-arrow-right`} />
                                         </Link>
@@ -71,7 +72,7 @@ const Nav = ({ translate, isLoggedIn, fullName }) => {
                 </li>
                 <li className="sep"></li>
                 <li>
-                    <Link to="/quotation-order">
+                    <Link to="/quotation-order" onClick={close}>
                         {translate("nav.quotationOrder")}
                     </Link>
 
@@ -80,11 +81,11 @@ const Nav = ({ translate, isLoggedIn, fullName }) => {
                     <a href="#">{translate("nav.consumableParts")}</a>
                     <ul className="cd-secondary-nav is-hidden">
                         <li className="go-back"><a href="#0">Back</a></li>
-                        <li><Link to="/listing?query=&page=1&category=2">{translate("nav.oilFilter")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=3">{translate("nav.airFilter")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=6">{translate("nav.brakePads")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=4">{translate("nav.acFilter")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=5">{translate("nav.sparkPlugs")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=2" onClick={close}>{translate("nav.oilFilter")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=3" onClick={close}>{translate("nav.airFilter")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=6" onClick={close}>{translate("nav.brakePads")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=4" onClick={close}>{translate("nav.acFilter")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=5" onClick={close}>{translate("nav.sparkPlugs")}</Link></li>
                     </ul>
                 </li>
                 <li className="has-children">
@@ -94,40 +95,40 @@ const Nav = ({ translate, isLoggedIn, fullName }) => {
                     </Link>
                     <ul className="cd-secondary-nav is-hidden">
                         <li className="go-back"><a href="#0">Back</a></li>
-                        <li><Link to="/listing?query=&page=1&category=7">{translate("nav.motorOil")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=8">{translate("nav.gearOil")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=27">{translate("nav.coolant")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=7" onClick={close}>{translate("nav.motorOil")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=8" onClick={close}>{translate("nav.gearOil")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=27" onClick={close}>{translate("nav.coolant")}</Link></li>
                     </ul>
                 </li>
 
-                <li><Link to="/listing?query=&page=1&category=13">{translate("nav.tires")}</Link></li>
+                <li><Link to="/listing?query=&page=1&category=13" onClick={close}>{translate("nav.tires")}</Link></li>
                 <li className="has-children">
                     <ul className="cd-secondary-nav is-hidden">
                         <li className="go-back"><a href="#0">Back</a></li>
-                        <li><Link to="/listing?query=&page=1&category=29">{translate("nav.handTools")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=30">{translate("nav.electricalTools")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=31">{translate("nav.tyreInflator")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=33">{translate("nav.repairEquipment")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=29" onClick={close}>{translate("nav.handTools")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=30" onClick={close}>{translate("nav.electricalTools")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=31" onClick={close}>{translate("nav.tyreInflator")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=33" onClick={close}>{translate("nav.repairEquipment")}</Link></li>
                     </ul>
                 </li>
                 <li className="has-children">
                     <a href="#">{translate("nav.accessorise")}</a>
                     <ul className="cd-secondary-nav is-hidden">
                         <li className="go-back"><a href="#0">Back</a></li>
-                        <li><Link to="/listing?query=&page=1&category=11">{translate("nav.carAccessorise")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=20">{translate("nav.exteriorAccessorise")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=17">{translate("nav.carMats")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=12">{translate("nav.wiresAndCables")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=18">{translate("nav.Covers")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=19">{translate("nav.carFirstAidKit")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=21">{translate("nav.InternalLights")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=22">{translate("nav.toolKits")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=23">{translate("nav.sunCurtains")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=24">{translate("nav.carSunShade")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=25">{translate("nav.towingTools")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=16">{translate("nav.bodyworkCleaningAndCare")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=14">{translate("nav.carRefrigerator")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=15">{translate("nav.childSeat")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=11" onClick={close}>{translate("nav.carAccessorise")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=20" onClick={close}>{translate("nav.exteriorAccessorise")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=17" onClick={close}>{translate("nav.carMats")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=12" onClick={close}>{translate("nav.wiresAndCables")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=18" onClick={close}>{translate("nav.Covers")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=19" onClick={close}>{translate("nav.carFirstAidKit")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=21" onClick={close}>{translate("nav.InternalLights")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=22" onClick={close}>{translate("nav.toolKits")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=23" onClick={close}>{translate("nav.sunCurtains")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=24" onClick={close}>{translate("nav.carSunShade")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=25" onClick={close}>{translate("nav.towingTools")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=16" onClick={close}>{translate("nav.bodyworkCleaningAndCare")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=14" onClick={close}>{translate("nav.carRefrigerator")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=15" onClick={close}>{translate("nav.childSeat")}</Link></li>
 
                     </ul>
                 </li>
@@ -135,12 +136,12 @@ const Nav = ({ translate, isLoggedIn, fullName }) => {
                     <a href="#">{translate("nav.carCare")}</a>
                     <ul className="cd-secondary-nav is-hidden">
                         <li className="go-back"><a href="#0">Back</a></li>
-                        <li><Link to="/listing?query=&page=1&category=34">{translate("nav.motorCareLiquids")}</Link></li>
-                        <li><Link to="/listing?query=&page=1&category=34">{translate("nav.washCleanersAndPolishers")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=34" onClick={close}>{translate("nav.motorCareLiquids")}</Link></li>
+                        <li><Link to="/listing?query=&page=1&category=34" onClick={close}>{translate("nav.washCleanersAndPolishers")}</Link></li>
                     </ul>
                 </li>
 
-                <li><Link to="/listing?query=&page=1&category=36">{translate("nav.sportsAndOutdoors")}</Link></li>
+                <li><Link to="/listing?query=&page=1&category=36" onClick={close}>{translate("nav.sportsAndOutdoors")}</Link></li>
                 <li className="sep"></li>
                 <li className="nav-sm has-children">
                     <a href="#">{translate("nav.country")}</a>
@@ -163,6 +164,10 @@ const Nav = ({ translate, isLoggedIn, fullName }) => {
             </ul>
         </nav>
     );
+}
+
+const close = () => {
+    closeNav();
 }
 
 export default Nav;
