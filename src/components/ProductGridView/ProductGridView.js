6ca457f086ccf2b5e99ce9d4a7c270a64ef8cac5
raedawwad95 <raed@qetaa.com>
2019-02-17 11:59:20 +0200

    fixed the mobile side

--- src/components/ProductGridView/ProductGridView.js
+++ src/components/ProductGridView/ProductGridView.js
@@ -3,6 +3,8 @@ import Stars from 'react-stars';
 import { starsRating } from '../../constants';
 import { getLength } from '../../utils/array';
 import Link from '../UI/Link';
+import { SmallScreen, MediumScreen } from '../Device/index.js';
+import { withRouter } from 'react-router-dom';
 
 class ProductGridView extends Component {
 	constructor(props) {
@@ -17,36 +19,66 @@ class ProductGridView extends Component {
 			isHovering: !this.state.isHovering
 		})
 	}
+	handleClick = (productId) => {
+		this.props.history.push(`/products/${productId}`)
+	}
 	render() {
 		const { product } = this.props;
-		return <div className="product-grid-view col-6 col-md-4" >
-			<div
-				className=" product-holder"
-				onMouseEnter={this.handleMouseHover}
-				onMouseLeave={this.handleMouseHover}>
-				<div className="image-container" align="center">
-					<img src={product.image} alt="no product" />
-					{
-						this.state.isHovering &&
-						<div className="product-buttons">
-							<Link to={`products/${product.id}`} className="btn btn-primary btn-detail" text="View Details" />
-							<Link to='#' className="btn btn-primary btn-cart" icons={["icon-cart", "icon-plus"]} />
+		return(
+			<Fragment>
+				<MediumScreen>
+					<div className="product-grid-view col-6 col-md-4" >
+						<div
+							className=" product-holder"
+							onMouseEnter={this.handleMouseHover}
+							onMouseLeave={this.handleMouseHover}>
+							<div className="image-container" align="center">
+								<img src={product.image} alt="no product" />
+								{
+									this.state.isHovering &&
+									<div className="product-buttons">
+										<Link to={`products/${product.id}`} className="btn btn-primary btn-detail" text="View Details" />
+										<Link to='#' className="btn btn-primary btn-cart" icons={["icon-cart", "icon-plus"]} />
+									</div>
+								}
+							</div>
+							<div className="details-holder">
+								<span className="part-text">{product.desc}</span><br />
+								<span className="manufacturer-text">{product.brand.name}</span>
+								<div className="product-review_slide">
+									<Stars values={product.averageRating} {...starsRating} />
+									<span className="product-review">{getLength(product.reviews)} review</span>
+								</div>
+								<span className="product-price">{product.salesPrice.toFixed(2)}</span>
+								<span className="product-currency">SR</span>
+							</div>
 						</div>
-					}
-				</div>
-				<div className="details-holder">
-					<span className="part-text">{product.desc}</span><br />
-					<span className="manufacturer-text">{product.brand.name}</span>
-					<div className="product-review_slide">
-						<Stars values={product.averageRating} {...starsRating} />
-						<span className="product-review">{getLength(product.reviews)} review</span>
 					</div>
-					<span className="product-price">{product.salesPrice.toFixed(2)}</span>
-					<span className="product-currency">SR</span>
-				</div>
-			</div>
-		</div>
+				</MediumScreen>
+				<SmallScreen>
+					<div className="product-grid-view col-6 col-md-4" >
+						<div
+							className=" product-holder"
+							onClick={() => this.handleClick(product.id)}>
+							<div className="image-container" align="center">
+								<img src={product.image} alt="no product" />
+							</div>
+							<div className="details-holder">
+								<span className="part-text">{product.desc}</span><br />
+								<span className="manufacturer-text">{product.brand.name}</span>
+								<div className="product-review_slide">
+									<Stars values={product.averageRating} {...starsRating} />
+									<span className="product-review">{getLength(product.reviews)} review</span>
+								</div>
+								<span className="product-price">{product.salesPrice.toFixed(2)}</span>
+								<span className="product-currency">SR</span>
+							</div>
+						</div>
+					</div>
+				</SmallScreen>
+			</Fragment>
+		)
 	}
 }
 
-export default ProductGridView;
+export default withRouter(ProductGridView);
