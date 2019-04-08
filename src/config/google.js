02dc3ac395aa12ab791aa7836ce5563586250f9b
AhmedShab <a.shaban@qetaa.com>
2019-04-08 15:50:49 +0300

    update google anl

--- src/config/google.js
+++ src/config/google.js
@@ -5,7 +5,7 @@ export const loadGoogleAnalytics = () => {
         ReactGA.initialize('UA-135621396-1');
         ReactGA.pageview(window.location.pathname + window.location.search);
     } else if (process.env.REACT_APP_NODE_ENV === 'production') {
-        ReactGA.initialize('GTM-KH9MT96');
+        ReactGA.initialize('UA-135621396-1');
         ReactGA.pageview(window.location.pathname + window.location.search);
     }
 }
