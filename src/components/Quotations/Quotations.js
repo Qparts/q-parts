a68026a3031e62be8d1f72422416b19a6c5fcac2
AhmedShab <a.shaban@qetaa.com>
2019-05-02 15:08:36 +0300

    update loading state

--- src/components/Quotations/Quotations.js
+++ src/components/Quotations/Quotations.js
@@ -18,12 +18,13 @@ class Quotations extends Component {
   constructor(props) {
     super(props);
     let query = queryString.parse(this.props.location.search.slice(1));
+    const isEmptyQuotation = _.isEmpty(props.quotations.pending && props.quotations.completed);
 
     this.state = {
       activeTab: '',
       noNewReply: '',
       show: parseInt(query.id, RADIX),
-      loading: true
+      loading: isEmptyQuotation ? true : false
     };
   }
 
@@ -35,157 +36,183 @@ class Quotations extends Component {
     this.setReadReplies();
     this.toggle(query.panel);
 
-        const that = this;
-    setTimeout(function(){ that.setState({loading: false}) }, 1000);
-  }
+    if (this.state.loading) {
+      setTimeout(() => {
+        this.setState({ loading: false })
+      }, 5000)
+    };
+}
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.props.quotations.completed !== prevProps.quotations.completed) {
-      this.setReadReplies();
+componentDidUpdate(prevProps, prevState) {
+  const { pending, completed } = this.props.quotations;
+  const prevPending = prevProps.quotations.pending;
+  const prevCompleted = prevProps.quotations.completed;
+
+  if (completed !== prevCompleted) {
+    this.setReadReplies();
+  }
+  const isEmptyQuotation = !_.isEmpty(pending || completed);
+
+  if ((prevPending !== pending || completed !== prevCompleted) && isEmptyQuotation) {
+    this.resetLoading();
+  }
+}
+
+resetLoading = async () => {
+  await this.setState({
+    loading: false
+  })
+}
+
+
+toggle = (tab = "pending") => {
+  if (this.state.activeTab !== tab) {
+    this.setState({
+      activeTab: tab
+    });
+  }
+}
+
+renderReviews = (array) => {
+  const { translate } = this.props;
+  return <Fragment>
+    <label htmlFor=""><b>{translate("setting.quotations.pendingRequest.table.comment")}</b></label> <br />
+    {
+      array.map((review, reviewKey) => {
+        return (
+          <Fragment key={reviewKey}>
+            {moment(review.created).format('MM/DD/YYYY')} {review.reviewText} <br />
+          </Fragment>
+        )
+      })
     }
-  }
+  </Fragment>
+}
+
+setReadReplies = () => {
+  let noNewReply = this.props.quotations.completed.every(reply => reply.read);
+
+  this.setState({
+    noNewReply: noNewReply ? '' : 'new'
+  })
+}
+
+render() {
+
+  const {
+    quotations, translate, currentLanguage, direction, addToCart, incrementQuantity,
+    decrementQuantity, token, customer, regions, putCompletedRequestRead
+  } = this.props;
+
+  let renderQuotation = null;
+
+  console.log('===========================================================================================');
+
+  console.log('this.state.loading: ', this.state.loading);
+  console.log('_.isEmpty(quotations.pending && quotations.completed): ', _.isEmpty(quotations.pending && quotations.completed));
+
+  console.log('===========================================================================================');
 
 
-  toggle = (tab = "pending") => {
-    if (this.state.activeTab !== tab) {
-      this.setState({
-        activeTab: tab
-      });
-    }
-  }
+  if (this.state.loading) {
+    return (
+      <div style={styles.loading}>
+        <ClipLoader
+          css={styles.spinner}
+          sizeUnit={"px"}
+          size={150}
+          loading={this.state.loading}
+        />
+      </div>
+    )
 
-  renderReviews = (array) => {
-    const { translate } = this.props;
-    return <Fragment>
-      <label htmlFor=""><b>{translate("setting.quotations.pendingRequest.table.comment")}</b></label> <br />
-      {
-        array.map((review, reviewKey) => {
-          return (
-            <Fragment key={reviewKey}>
-              {moment(review.created).format('MM/DD/YYYY')} {review.reviewText} <br />
-            </Fragment>
-          )
-        })
-      }
+  } else if (_.isEmpty(quotations.pending && quotations.completed)) {
+    renderQuotation = <div className="empty">
+      <LargeScreen>
+        <header>
+          <h5>3 {translate("setting.quotations.steps")}</h5>
+        </header>
+      </LargeScreen>
+      <figure>
+        <img src="/img/request.svg" alt="no img" />
+      </figure>
+      <figcaption>
+        <p>{translate("setting.quotations.empty")} </p>
+        <Link className="btn btn-primary" to="/quotation-order" >{translate("quotationOrder.startHere")} <i className={`icon-arrow-${right(direction)}`}></i></Link>
+      </figcaption>
+    </div>
+
+  } else {
+    renderQuotation = <Fragment>
+      <Nav tabs>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: this.state.activeTab === 'pending' })}
+            onClick={() => { this.toggle('pending'); }}>
+            {translate("quotationRequest.pending")}
+          </NavLink>
+        </NavItem>
+        <NavItem className={this.state.noNewReply}>
+          <NavLink
+            className={classnames({ active: this.state.activeTab === 'replied' })} onClick={() => { this.toggle('replied'); }} >
+            {translate("quotationRequest.replied")} <span></span>
+          </NavLink>
+        </NavItem>
+      </Nav>
+      <TabContent activeTab={this.state.activeTab}>
+        <TabPane tabId="pending">
+          <ul className="list-unstyled" id="request-list">
+            {
+              quotations.pending.map(pending => {
+                return <PendingRequest
+                  key={pending.id}
+                  pending={pending}
+                  translate={translate}
+                  currentLanguage={currentLanguage}
+                  vehicles={customer.vehicles}
+                  regions={regions}
+                />
+              })
+            }
+          </ul>
+        </TabPane>
+        <TabPane tabId="replied">
+          <ul className="list-unstyled" id="replied-list">
+            {
+              quotations.completed.map((reply, completedIndex) => {
+                return <CompletedRequest
+                  key={reply.id}
+                  show={this.state.show}
+                  completedIndex={completedIndex}
+                  reply={reply}
+                  translate={translate}
+                  currentLanguage={currentLanguage}
+                  addToCart={addToCart}
+                  incrementQuantity={incrementQuantity}
+                  decrementQuantity={decrementQuantity}
+                  direction={direction}
+                  token={token}
+                  vehicles={customer.vehicles}
+                  regions={regions}
+                  putCompletedRequestRead={putCompletedRequestRead}
+                />
+              })
+            }
+          </ul>
+        </TabPane>
+      </TabContent>
     </Fragment>
   }
 
-  setReadReplies = () => {
-    let noNewReply = this.props.quotations.completed.every(reply => reply.read);
-
-    this.setState({
-      noNewReply: noNewReply ? '' : 'new'
-    })
-  }
-
-  render() {
-
-    const {
-      quotations, translate, currentLanguage, direction, addToCart, incrementQuantity,
-      decrementQuantity, token, customer, regions, putCompletedRequestRead, isLoading
-    } = this.props;
-
-    let renderQuotation = null;
-
-    if (_.isEmpty(quotations.pending && quotations.completed)) {
-      renderQuotation = <div className="empty">
-        <LargeScreen>
-          <header>
-            <h5>3 {translate("setting.quotations.steps")}</h5>
-          </header>
-        </LargeScreen>
-        <figure>
-          <img src="/img/request.svg" alt="no img" />
-        </figure>
-        <figcaption>
-          <p>{translate("setting.quotations.empty")} </p>
-          <Link className="btn btn-primary" to="/quotation-order" >{translate("quotationOrder.startHere")} <i className={`icon-arrow-${right(direction)}`}></i></Link>
-        </figcaption>
-      </div>
-
-    } else {
-      renderQuotation = <Fragment>
-        <Nav tabs>
-          <NavItem>
-            <NavLink
-              className={classnames({ active: this.state.activeTab === 'pending' })}
-              onClick={() => { this.toggle('pending'); }}>
-              {translate("quotationRequest.pending")}
-            </NavLink>
-          </NavItem>
-          <NavItem className={this.state.noNewReply}>
-            <NavLink
-              className={classnames({ active: this.state.activeTab === 'replied' })} onClick={() => { this.toggle('replied'); }} >
-              {translate("quotationRequest.replied")} <span></span>
-            </NavLink>
-          </NavItem>
-        </Nav>
-        <TabContent activeTab={this.state.activeTab}>
-          <TabPane tabId="pending">
-            <ul className="list-unstyled" id="request-list">
-              {
-                quotations.pending.map(pending => {
-                  return <PendingRequest
-                    key={pending.id}
-                    pending={pending}
-                    translate={translate}
-                    currentLanguage={currentLanguage}
-                    vehicles={customer.vehicles}
-                    regions={regions}
-                  />
-                })
-              }
-            </ul>
-          </TabPane>
-          <TabPane tabId="replied">
-            <ul className="list-unstyled" id="replied-list">
-              {
-                quotations.completed.map((reply, completedIndex) => {
-                  return <CompletedRequest
-                    key={reply.id}
-                    show={this.state.show}
-                    completedIndex={completedIndex}
-                    reply={reply}
-                    translate={translate}
-                    currentLanguage={currentLanguage}
-                    addToCart={addToCart}
-                    incrementQuantity={incrementQuantity}
-                    decrementQuantity={decrementQuantity}
-                    direction={direction}
-                    token={token}
-                    vehicles={customer.vehicles}
-                    regions={regions}
-                    putCompletedRequestRead={putCompletedRequestRead}
-                  />
-                })
-              }
-            </ul>
-          </TabPane>
-        </TabContent>
-      </Fragment>
-    }
-
-      if ( this.state.loading) {
-        return (
-          <div style={styles.loading}>
-            <ClipLoader
-              css={styles.spinner}
-              sizeUnit={"px"}
-              size={150}
-              loading={isLoading}
-            />
-          </div>
-        )
-      }
-    return (
-      <div className="requests-main">
-        <DownLargeScreen>
-          <h2 className="sec-title-sm">{translate("setting.quotations.title")}</h2>
-        </DownLargeScreen>
-        {renderQuotation}
-      </div>
-    )
-  }
+  return (
+    <div className="requests-main">
+      <DownLargeScreen>
+        <h2 className="sec-title-sm">{translate("setting.quotations.title")}</h2>
+      </DownLargeScreen>
+      {renderQuotation}
+    </div>
+  )
+}
 }
 
 
