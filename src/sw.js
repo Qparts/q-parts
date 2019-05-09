01a46d6bf3315f74fc191dddc38794d02591d476
AhmedShab <a.shaban@qetaa.com>
2019-05-09 15:46:42 +0300

    add a custom SW

---
+++ src/sw.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-undef */
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+
+self.addEventListener('push', (event) => {
+  const title = 'Get Started With Workbox';
+  const options = {
+    body: event.data.text()
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
