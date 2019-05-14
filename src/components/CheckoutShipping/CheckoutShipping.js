6afaf8d43223da7f1f569c56241adb3cf1ef6200
raedawwad95 <raed@qetaa.com>
2019-05-14 16:09:00 +0200

    check if we dont have any addresses

--- src/components/CheckoutShipping/CheckoutShipping.js
+++ src/components/CheckoutShipping/CheckoutShipping.js
@@ -49,11 +49,27 @@ class CheckoutShipping extends Component {
   }
   handleDelivery = e => {
     const { deliveryAddress } = this.props.checkout;
-
     if (Object.keys(deliveryAddress).length > 0) {
       this.props.completeShipping(true);
       this.props.history.push('/checkout/payment');
     }
+  }
+  handleDeliveryNoAddress = values =>{
+    console.log(values)
+    const { line1, line2, zipCode, title, mobile, city, defaultAddress } = values;
+    const latitude = city.latitude;
+    const longitude = city.longitude;
+    const cityId = city.id;
+    this.props.addAddress({ line1, line2, cityId, zipCode, title, latitude, longitude, mobile, defaultAddress: _.isUndefined(defaultAddress) ? false : defaultAddress })
+      .then(() => {
+        this.setState({ hasNewAddress: false });
+          const { deliveryAddress } = this.props.checkout;
+          console.log(deliveryAddress)
+          if (Object.keys(deliveryAddress).length > 0) {
+            this.props.completeShipping(true);
+            this.props.history.push('/checkout/payment');
+          }
+      })
   }
   handleAddNewAddress = () => {
     this.setState({ hasNewAddress: true });
@@ -68,6 +84,7 @@ class CheckoutShipping extends Component {
   }
   handleSubmit = values => {
     const { line1, line2, zipCode, title, mobile, city, defaultAddress } = values;
+    console.log(values)
     const latitude = city.latitude;
     const longitude = city.longitude;
     const cityId = city.id;
@@ -163,10 +180,9 @@ class CheckoutShipping extends Component {
             readOnly />
         </div>
       </Fragment>
-
-    return (
-      <Fragment>
-        {
+      let haveAddress;
+      if(addresses){
+        haveAddress =<Fragment>{
           this.props.showGoogleMap ? (
             <div className="Address-map">
               <Map className="map" google={this.props.google} visible={false}>
@@ -179,7 +195,7 @@ class CheckoutShipping extends Component {
             <div className="Address-container col-12 col-md-9">
               <div className="addresses-header justify-content-between ">
                 <p>{translate("setting.addressBook.shippingItem")}</p>
-                <Button type="button" className="btn btn-primary" icon="icon-add" text={translate("setting.addressBook.add")} onClick={this.handleAddNewAddress} isReverseOrder />
+                <Button type="button" className={this.state.hasNewAddress? "hide" : "btn btn-primary"} icon="icon-add" text={translate("setting.addressBook.add")} onClick={this.handleAddNewAddress} isReverseOrder />
               </div>
               <form onSubmit={handleSubmit(this.handleSubmit)}>
                 {(
@@ -267,6 +283,107 @@ class CheckoutShipping extends Component {
               </form>
             </div>
         }
+      </Fragment>
+    }else{
+      haveAddress=<Fragment>{
+        this.props.showGoogleMap ? (
+          <div className="Address-map">
+            <Map className="map" google={this.props.google} visible={false}>
+              {
+                this.props.country && <AutoComplete onAddressSelected={this.onAddressSelected} {...this.props} />
+              }
+            </Map>
+          </div>
+        ) :
+          <div className="Address-container col-12 col-md-9">
+            <div className="addresses-header justify-content-between ">
+              <p>{translate("setting.addressBook.shippingItem")}</p>
+            </div>
+            <form onSubmit={handleSubmit(this.handleDeliveryNoAddress)}>
+               <div className="row no-gutters">
+                  <div className="col-12 title-address">
+                    <Field
+                      label={translate("setting.addressBook.title")}
+                      name="title"
+                      component={RenderField}
+                      type="text"
+                      placeholder={translate("setting.addressBook.title")}
+                      hasFloatLabel
+                      validate={[validations.required]} />
+                  </div>
+                  {/* <div className="col-12 google-map">
+                  <Link to="#" onClick={onShowGoogleMap}>
+                    <img className="main-img" alt="user" src="/img/google-map.svg" />
+                    <p>{translate("form.address.selectAddress")}</p>
+                  </Link>
+                </div> */}
+                  <div className="col-md-12 address-title">
+                    <Field
+                      label={translate("setting.addressBook.addressLine1")}
+                      name="line1"
+                      component={RenderField}
+                      type="text"
+                      placeholder={translate("setting.addressBook.addressLine1")}
+                      hasFloatLabel
+                      validate={[validations.required]} />
+                  </div>
+                  <div className="col-12 address-title">
+                    <Field
+                      label={translate("setting.addressBook.addressLine2")}
+                      name="line2"
+                      component={RenderField}
+                      type="text"
+                      placeholder={translate("setting.addressBook.addressLine2")}
+                      hasFloatLabel />
+                  </div>
+                  {renderCityRegion}
+                  <div className="phone-info col-12">
+                    <div className="row">
+                      <div className="phone-number col-12">
+                        <div className="first">
+                          <input
+                            className="form-control"
+                            value={"+966"}
+                            type="text"
+                            readOnly />
+                        </div>
+                        <Field
+                          name="mobile"
+                          component={RenderField}
+                          placeholder={translate("form.address.phoneNumber")}
+                          validate={[validations.required]} />
+                      </div>
+                    </div>
+                    <div className="phone-info col-12">
+                      <div className="row">
+                        <div className="zipCode col-12">
+                          <Field
+                            name="zipCode"
+                            component={RenderField}
+                            placeholder={translate("form.address.zipCode")}
+                            validate={[validations.required]} />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-12 shipping-style">
+                    <Field
+                      name="shipping"
+                      component={RenderField}
+                      type="text"
+                      placeholder={translate("form.address.shippingNote")} />
+                  </div>
+                </div>
+
+              <Button type="submit" className="btn btn-secondary" text={translate("form.address.buttons.deliver")} icon={`icon-arrow-${right(direction)}`} />
+            </form>
+          </div>
+      }
+    </Fragment>
+    }
+    return (
+      <Fragment>
+        {haveAddress}
       </Fragment>
     )
   }
